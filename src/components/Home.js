f7a0bbb783dd06e424f020e17c10f90b9feeb8ca
Prabir Singh <codeprabir@gmail.com>
2022-02-16 09:22:14 +0530

    ADD: Section for the TESLA

---
+++ src/components/Home.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
+
+function Home() {
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        LeftBtnText="Custom Order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-y.jpg"
+        LeftBtnText="Custom Order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-3.jpg"
+        LeftBtnText="Custom Order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-x.jpg"
+        LeftBtnText="Custom Order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Lowest Cost Solar Panels in America"
+        description="Mone-back gurantee"
+        backgroundImg="solar-panel.jpg"
+        LeftBtnText="Order Now"
+        rightBtnText="Learn more"
+      />
+      <Section
+        title="Solar for New Roofs"
+        description="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+        backgroundImg="solar-roof.jpg"
+        LeftBtnText="Order Now"
+        rightBtnText="Learn more"
+      />
+      <Section
+        title="Accesories"
+        description=""
+        backgroundImg="accessories.jpg"
+        LeftBtnText="Shop Now"
+        
+      />
+    </Container>
+  )
+}
+
+export default Home
+
+const Container = styled.div`
+  height: 100vh;
+`
